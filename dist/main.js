e71257790de50d3b47fc83a0565650de944ae7d9
diegoargottez@gmail.com <diegoargottez@gmail.com>
2024-01-31 11:57:44 -0400

    del]: dist from the github repo

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-const register_command_1 = require("./register-command");
-const registerCommand = new register_command_1.RegisterCommand();
-const bot = new index_1.BotDiscord(registerCommand);
-bot.start();
